95397a00f86477267dc371cbe186b20a3b189056
ozgucdlg <ozgucdalga@gmail.com>
2023-08-16 11:39:57 -0400

    Editted Contact page

--- myapp/src/app/components/contact/contact.component.ts
+++ myapp/src/app/components/contact/contact.component.ts
@@ -1,10 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-contact',
   templateUrl: './contact.component.html',
   styleUrls: ['./contact.component.css']
 })
-export class ContactComponent {
+export class ContactComponent implements OnInit {
+
+  const toastTrigger = document.getElementById('liveToastBtn')
+  const toastLiveExample = document.getElementById('liveToast')
+  ngOnInit(): void {
+    throw new Error('Method not implemented.');
+  }
+
+
+
+if (toastTrigger: { addEventListener: (arg0: string, arg1: () => void) => void; }) {
+  const toastBootstrap = bootstrap.Toast.getOrCreateInstance(this.toastLiveExample)
+  toastTrigger.addEventListener('click', () => {
+    toastBootstrap.show()
+  })
+}
 
 }
